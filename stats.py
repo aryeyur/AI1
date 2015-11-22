0a393fbb1499bd5c0750d4ecdccc5f5953deab7c
ayurkovs <arye@campus.technion.ac.il>
2015-11-22 19:37:37 +0200

    Qestion 1 part 2

--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ This file should be runnable to print map_statistics using
 $ python stats.py
 '''
 
-from collections import namedtuple
+from collections import namedtuple, Counter
 from ways import load_map_from_csv
 
 
@@ -11,14 +11,19 @@ def map_statistics(roads):
     '''return a dictionary containing the desired information
     You can edit this function as you wish'''
     Stat = namedtuple('Stat', ['max', 'min', 'avg'])
+
     return {
-        'Number of junctions' : None,
-        'Number of links' : None,
-        'Outgoing branching factor' : Stat(max=None, min=None, avg=None),
-        'Link distance' : Stat(max=None, min=None, avg=None),
+        'Number of junctions' : len(roads.junctions()),
+        'Number of links' : sum(1 for x in roads.iterlinks()),
+        'Outgoing branching factor' : Stat(max=max(len(x.links) for x in roads.junctions()),
+                                           min=min(len(x.links) for x in roads.junctions()),
+                                           avg=sum(len(x.links) for x in roads.junctions()) / sum(1 for x in roads.junctions())),
+        'Link distance' : Stat(max=max(x.distance for x in roads.iterlinks()),
+                               min=min(x.distance for x in roads.iterlinks()),
+                               avg=sum(x.distance for x in roads.iterlinks()) / sum(1 for x in roads.iterlinks())),
         # value should be a dictionary
         # mapping each road_info.TYPE to the no' of links of this type
-        'Link type histogram' : None,  # tip: use collections.Counter
+        'Link type histogram' : Counter(x.highway_type for x in roads.iterlinks()).items(),  # tip: use collections.Counter
     }
 
 
