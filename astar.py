57f0091f258b384307b17792ad48d53f77f79f33
Max yor <maxyor6040@gmail.com>
2015-12-02 22:53:01 +0200

    minor bug fix #2

--- astar.py
+++ astar.py
@@ -1,6 +1,5 @@
 import sys
 from collections import namedtuple
-
 from ways import load_map_from_csv, compute_distance
 
 Node = namedtuple('Node', ['junction', 'parent', 'g_value', 'h_value', 'f_value', ])
@@ -26,9 +25,9 @@ def children_of_junction(junction):
     return children
 
 
-def list_find_node(node_list, node):
+def list_find_node(node_list, junction):
     for x in node_list:
-        if x.junction == node.junction:
+        if x.junction == junction:
             return x
     return None
 
@@ -48,7 +47,9 @@ def format_result(last_node):
     top = last_node
     all_indices = list()
     while top is not None:
-        all_indices.append(top.index)
+        all_indices.append(top.junction.index)
+        top = top.parent
+    return all_indices
 
 
 def run_astar(source_junction_index, target_junction_index, t0, price_func=price_function,
@@ -63,30 +64,41 @@ def run_astar(source_junction_index, target_junction_index, t0, price_func=price
     source_h_value = heuristic_func(source_junction, target_junction, t0)
     open_list.append(Node(source_junction, None, 0, source_h_value, source_h_value))
 
-    while not open_list:  # while list is not empty
+    while open_list:  # while list is not empty
         best_node = get_best_node(open_list)
+        open_list.remove(best_node)
         closed_list.append(best_node)
         if best_node.junction == target_junction:
             return format_result(best_node)
-        for son in children_of_junction(best_node):
-            son_g_value = price_func(best_node, son, t0) + best_node.g_value
+        for son in children_of_junction(best_node.junction):
+            son_g_value = price_func(best_node.junction, son, t0) + best_node.g_value
             son_in_open = list_find_node(open_list, son)
             if son_in_open is not None:
                 if son_in_open.g_value > son_g_value:
-                    son_in_open.g_value = son_g_value
-                    son_in_open.h_value = heuristic_func(son_in_open, target_junction, t0)
-                    son_in_open.f_value = son_in_open.g_value + son_in_open.h_value
-                    son_in_open.parent = best_node
+                    son_copy = Node(son_in_open.junction, best_node, son_g_value,
+                                    heuristic_func(son_in_open.junction, target_junction, t0),
+                                    heuristic_func(son_in_open.junction, target_junction, t0) + son_g_value)
+                    open_list.remove(son_in_open)
+                    open_list.append(son_copy)
+                    # son_in_open.g_value = son_g_value
+                    # son_in_open.h_value = heuristic_func(son_in_open.junction, target_junction, t0)
+                    # son_in_open.f_value = son_in_open.g_value + son_in_open.h_value
+                    # son_in_open.parent = best_node
                 continue
             son_in_closed = list_find_node(closed_list, son)
             if son_in_closed is not None:
                 if son_in_closed.g_value > son_g_value:
-                    closed_list.remove(son_in_closed)
-                    son_in_closed.g_value = son_g_value
-                    son_in_closed.h_value = heuristic_func(son_in_open, target_junction, t0)
-                    son_in_closed.f_value = son_in_open.g_value + son_in_open.h_value
-                    son_in_closed.parent = best_node
-                    open_list.append(son_in_closed)
+                    son_copy = Node(son_in_closed.junction, best_node, son_g_value,
+                                    heuristic_func(son_in_open.junction, target_junction, t0),
+                                    heuristic_func(son_in_open.junction, target_junction, t0) + son_g_value)
+                    open_list.remove(son_in_closed)
+                    open_list.append(son_copy)
+                    # closed_list.remove(son_in_closed)
+                    # son_in_closed.g_value = son_g_value
+                    # son_in_closed.h_value = heuristic_func(son_in_open.junction, target_junction, t0)
+                    # son_in_closed.f_value = son_in_open.g_value + son_in_open.h_value
+                    # son_in_closed.parent = best_node
+                    # open_list.append(son_in_closed)
                 continue
             son_h_value = heuristic_func(son, target_junction, t0)
             open_list.append(Node(son, best_node, son_g_value, son_h_value, son_g_value + son_h_value))
