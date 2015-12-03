610d6580fa0081885a8a0c83adfe5328f81937b0
Max yor <maxyor6040@gmail.com>
2015-12-03 17:24:42 +0200

    working part 2

--- astar.py
+++ astar.py
@@ -3,8 +3,7 @@ from collections import namedtuple
 from ways import load_map_from_csv, compute_distance
 from ways.info import SPEED_RANGES
 from ways.tools import base_traffic_pattern
-import pqdict
-
+from pqdict import pqdict
 
 Node = namedtuple('Node', ['junction', 'parent', 'g_value', 'h_value', 'f_value', ])
 
@@ -30,32 +29,17 @@ def children_of_junction(junction):
     return children
 
 
-def list_find_node(node_list, junction):
-    for x in node_list:
-        if x.junction.index == junction.index:
-            return x
-    return None
-
-
 def price_function(source_junction, target_junction, t0):
     global roads
     for lnk in source_junction.links:
         if lnk.target == target_junction.index:
-            #print(
-            #    'price_function from: ' + str(source_junction.index) + ' to ' + str(
-            #        target_junction.index) + ' is [' + str((lnk.distance/1000) / roads.realtime_link_speed(
-            #        lnk, t0)) + '].')  # TODO remove this. for debug only
-            return (lnk.distance/1000) / roads.realtime_link_speed(lnk, t0)
+            return (lnk.distance / 1000) / roads.realtime_link_speed(lnk, t0)
     return sys.maxsize
 
 
 def heuristic_function(source_junction, target_junction, t0):
-    #print(
-    #    'heuristic_function from: ' + str(source_junction.index) + ' to ' + str(target_junction.index) + ' is [' + str(
-    #        compute_distance(
-    #            source_junction.lat, source_junction.lon, target_junction.lat,
-    #            target_junction.lon) / 110) + '].')  # TODO remove this. for debug only
-    return compute_distance(source_junction.lat, source_junction.lon, target_junction.lat, target_junction.lon) / 110 #110 is the highest speed
+    return compute_distance(source_junction.lat, source_junction.lon, target_junction.lat,
+                            target_junction.lon) / 110  # 110 is the highest speed
 
 
 def format_result(last_node):
@@ -68,44 +52,43 @@ def format_result(last_node):
     return all_indices
 
 
+def comparator_f(node1, node2):
+    return node1.f_value < node2.f_value
+
+
 def run_astar(source_junction_index, target_junction_index, t0, price_func=price_function,
               heuristic_func=heuristic_function):
     global roads
     roads = load_map_from_csv()
     source_junction = roads.junctions()[source_junction_index]
     target_junction = roads.junctions()[target_junction_index]
-    closed_list = list()
-    open_list = list()
+    closed_list = dict()
+    open_pqdict = pqdict(precedes=comparator_f)
 
     source_h_value = heuristic_func(source_junction, target_junction, t0)
-    open_list.append(Node(source_junction, None, 0, source_h_value, source_h_value))
+    open_pqdict.additem(source_junction_index, Node(source_junction, None, 0, source_h_value, source_h_value))
 
-    while open_list:  # while list is not empty
-        best_node = get_best_node(open_list)
-        open_list.remove(best_node)
-        closed_list.append(best_node)
-        if best_node.junction.index == target_junction.index:
+    while open_pqdict:  # while list is not empty
+        best_node = open_pqdict.popitem()[1]
+        closed_list[best_node.junction.index] = best_node
+        if best_node.junction.index == target_junction_index:
             return format_result(best_node)
         for son in children_of_junction(best_node.junction):
             son_g_value = price_func(best_node.junction, son, t0) + best_node.g_value
-            son_in_open = list_find_node(open_list, son)
-            if not (son_in_open is None):
-                if son_in_open.g_value > son_g_value:
-                    son_copy = Node(son_in_open.junction, best_node, son_g_value,
-                                    heuristic_func(son_in_open.junction, target_junction, t0),
-                                    heuristic_func(son_in_open.junction, target_junction, t0) + son_g_value)
-                    open_list.remove(son_in_open)
-                    open_list.append(son_copy)
-                continue
-            son_in_closed = list_find_node(closed_list, son)
-            if not (son_in_closed is None):
-                if son_in_closed.g_value > son_g_value:
-                    son_copy = Node(son_in_closed.junction, best_node, son_g_value,
-                                    heuristic_func(son_in_open.junction, target_junction, t0),
-                                    heuristic_func(son_in_open.junction, target_junction, t0) + son_g_value)
-                    closed_list.remove(son_in_closed)
-                    open_list.append(son_copy)
-                continue
             son_h_value = heuristic_func(son, target_junction, t0)
-            open_list.append(Node(son, best_node, son_g_value, son_h_value, son_g_value + son_h_value))
+            if son.index in open_pqdict.keys():
+                if open_pqdict[son.index].g_value > son_g_value:
+                    son_copy = Node(son, best_node, son_g_value, son_h_value,
+                                    son_h_value + son_g_value)
+                    open_pqdict.updateitem(son.index, son_copy)
+                continue
+
+            if son.index in closed_list:
+                if closed_list[son.index].g_value > son_g_value:
+                    son_copy = Node(son, best_node, son_g_value, son_h_value,
+                                    son_h_value + son_g_value)
+                    del closed_list[son.index]
+                    open_pqdict.additem(son_copy.junction.index, son_copy)
+                continue
+            open_pqdict.additem(son.index, Node(son, best_node, son_g_value, son_h_value, son_g_value + son_h_value))
     return None
